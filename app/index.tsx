03e7acabb40cb45f8923600359a1002556546ee4
Daniel Lima <danielfbl@gmail.com>
2024-09-12 14:25:22 -0300

    initializing... installing libs and working on home background

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+
+import HomeBackground from "@/components/HomeBackground";
+
+export default function Index() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <HomeBackground />
+    </>
+  );
+}

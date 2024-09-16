a243e67f31a007a555351f0104358656c42258cd
Daniel Lima <danielfbl@gmail.com>
2024-09-16 14:36:33 -0300

    finishing Tab Bar

--- components/tabBar/elements/TabBarItems.tsx
+++ components/tabBar/elements/TabBarItems.tsx
@@ -1,8 +1,16 @@
-import { View } from "react-native";
+import useApplicationDimensions from "@/hooks/useApplicationDimensions";
+import { Pressable, StyleSheet, View } from "react-native";
 import ListIcon from "../icons/ListIcon";
 import MapIcon from "../icons/MapIcon";
+import CircleButton from "./CircleButton";
+import TrapezoidBackground from "./TrapezoidBackground";
 
 export default function TabBarItems() {
+  const { width, height } = useApplicationDimensions();
+  const trapezoidWidth = width * 0.68;
+  const trapezoidHeight = height * 0.12;
+  const circleRadius = (trapezoidHeight * 0.51) / 2;
+  const buttonCenterX = width / 2 - circleRadius;
   return (
     <View
       style={{
@@ -14,6 +22,20 @@ export default function TabBarItems() {
       }}
     >
       <MapIcon />
+      <TrapezoidBackground width={trapezoidWidth} height={trapezoidHeight} />
+      <Pressable
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          left: buttonCenterX,
+          top: 12,
+          width: circleRadius * 2,
+          height: circleRadius * 2,
+        }}
+      >
+        {({ pressed }) => (
+          <CircleButton radius={circleRadius} pressed={pressed} />
+        )}
+      </Pressable>
       <ListIcon />
     </View>
   );
